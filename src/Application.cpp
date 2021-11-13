ad149b2992a73cd6510d8f515dc7c1e467885289
AMS21 <19614997+AMS21@users.noreply.github.com>
2021-11-13 12:40:08 +0100

    [Test] Add Livetests for CppCheck
    Currently only tested with CI on Linux

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,9 +1,92 @@
 #include "Application.hpp"
 
+#include "Location.hpp"
+#include "Parser/CppCheck.hpp"
+#include "Warning.hpp"
+#include "WarningDatabase.hpp"
+#include <fstream>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <string_view>
+
 namespace chw
 {
+    [[nodiscard]] std::optional<std::string> ReadFileContent(
+            const std::string_view& file_path) noexcept
+    {
+        std::FILE* file = fopen(file_path.data(), "r");
+
+        if (file != nullptr)
+        {
+            std::fseek(file, 0, SEEK_END);
+
+            std::size_t size = std::ftell(file);
+            if (size != -1)
+            {
+                std::rewind(file);
+
+                // Create empty string of desired size
+                std::string str(size, '\0');
+
+                // Read data
+                std::fread(&str[0], sizeof(std::string::value_type), size, file);
+
+                std::fclose(file);
+
+                return std::move(str);
+            }
+            else
+            {
+                std::fclose(file);
+                return {};
+            }
+        }
+
+        return {};
+    }
+
+    void PrintDatabase(WarningDatabase& database) noexcept
+    {
+        for (const Warning& warn : database.m_Warnings)
+        {
+            const Location& loc = warn.GetLocation();
+            // Location
+            std::cout << loc.GetFileName() << ":" << loc.GetLineNumber() << ":" << loc.GetColumn();
+            // Warnings
+            std::cout << " (" << warn.GetSeverity() << "): " << warn.GetMessage() << " ["
+                      << warn.GetId() << "]\n";
+        }
+    }
+
     int Application::Run(int argc, char* argv[])
     {
+        if (argc < 2)
+        {
+            // TODO: print usage
+
+            return 0;
+        }
+
+        // Treat first input as the file we want to check
+        const char* file_name = argv[1];
+
+        auto file_content_opt = ReadFileContent(file_name);
+        if (!file_content_opt)
+        {
+            std::cerr << "Failed to open file \"" << file_name << "\"\n";
+            return -1;
+        }
+
+        std::string file_content = file_content_opt.value();
+
+        // Create warning database
+        WarningDatabase database;
+
+        ParseCppCheckOutput(file_content, database);
+
+        PrintDatabase(database);
+
         return 0;
     }
 } // namespace chw
---
+++ tests/Livetests/src/CppCheck.live.test.cpp
@@ -0,0 +1,31 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include <Application.hpp>
+#include <iostream>
+#include <sstream>
+
+static constexpr const char* expected_cppcheck_output{
+        R"(MemoryLeaks/MallocLeak.c:6:1 (error): Memory leak: ptr [memleak]
+MemoryLeaks/NewLeak.cpp:6:1 (error): Memory leak: i [memleak]
+OutOfBounds/IndexOutOfBounds.c:7:10 (error): Array 'a[2]' accessed at index 2, which is out of bounds. [arrayIndexOutOfBounds]
+OutOfBounds/strcpy.c:6:12 (error): Buffer is accessed out of bounds: x [bufferAccessOutOfBounds]
+ResourceLeak/FileHandle.c:11:5 (error): Resource leak: handle [resourceLeak]
+SyntaxErrors/MissingClosingBracket.c:2:1 (error): Unmatched '{'. Configuration: ''. [syntaxError]
+)"};
+
+TEST_CASE("Livetest Cppcheck")
+{
+    std::ostringstream out;
+    auto*              original = std::cout.rdbuf();
+    std::cout.rdbuf(out.rdbuf());
+
+    chw::Application app;
+
+    char* argv[2] = {"", DATA_DIRECTORY "/CppCheck.output"};
+
+    app.Run(2, argv);
+
+    std::cout.rdbuf(original);
+
+    CHECK(out.str() == expected_cppcheck_output);
+}
